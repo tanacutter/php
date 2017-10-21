3ed9c1589baa3b135717547644708115b26423ee
tanacutter <tanaka.hrnb@gmail.com>
2017-10-21 22:02:01 +0900

    home,user コントローラ削除

--- routes/web.php
+++ routes/web.php
@@ -17,8 +17,6 @@ Route::resource('/', 'TopController');
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
-
 Route::resource('users', 'UserController');
 
 Route::resource('calendars', 'CalendarController');
